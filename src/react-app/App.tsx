4e488a6e5847e038e47c4338e159cfa94f68d553
suen-droid <sueny530@gmail.com>
2026-06-26 10:50:53 +0900

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const images = [1, 2, 3, 4, 6, 7, 8, 9, 10];
+  const images = [1, 3, 4, 6, 7, 8, 9, 10];
 
   const [current, setCurrent] = useState<number | null>(null);
 
